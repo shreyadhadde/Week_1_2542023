b57211e6b561acf3a3f08a479b2a4a81e00751db
shreyadhadde <shreyadhadde5@gmail.com>
2026-01-03 21:54:46 +0530

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,17 @@
+def tri_area():
+	b=9
+	h=3
+	print("Area of triangle is:",0.5*b*h)
+
+def circle_area():
+	r=5
+	print("Area of circle is:",3.14*r*r)
+
+def rect_area():
+	l=20
+	b=5
+	print("Area of rectangle is:",l*b)
+
+def square_area():
+	side=5
+	print("Area of square is:",side*side)			
---
+++ demo1.py
@@ -0,0 +1,6 @@
+from demo import *
+
+tri_area()
+circle_area()
+rect_area()
+square_area()
